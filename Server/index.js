8e998308e7c2541db5055c78c44e74551b27e0d4
Sanzina <sanzina67shorna@gmail.com>
2025-01-01 16:55:54 +0600

    server changes

--- Server/index.js
+++ Server/index.js
@@ -1,26 +1,37 @@
 import { ApolloServer } from '@apollo/server';
 import { startStandaloneServer } from '@apollo/server/standalone';
 
-// Define your schema (type definitions)
 const typeDefs = `
-  type Query {
-    hello: String
-  }
-`;
+type Query
+{
 
-// Define your resolvers
+}
+
+type Mutation
+{
+
+}
+
+type users
+{
+    id: ID
+    name:String
+    age: Int
+    isMarried: Boolean
+}
+
+`
 const resolvers = {
-  Query: {
-    hello: () => 'Hello, world!',
-  },
-};
+  
+}
 
 // Create the Apollo Server instance
-const server = new ApolloServer({ typeDefs, resolvers });
+const server = new ApolloServer({typeDefs,resolvers });
 
 // Start the server
-startStandaloneServer(server, {
+const { url } = await startStandaloneServer(server, {
   listen: { port: 5000 },
-}).then(({ url }) => {
-  console.log(`Congratulation !!! GraphQL server is ready to work at ${url}`);
 });
+
+console.log(`Congratulation !!! GraphQL server is ready to work at ${url}`);
+
