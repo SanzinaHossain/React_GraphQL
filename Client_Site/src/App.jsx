ccdc675b1bc8ac5902b39097c526ee30ee79d2a3
Sanzina <sanzina67hossain@gmail.com>
2025-01-02 16:21:25 +0600

    integrate and fetch data from server side using graphql

--- Client_Site/src/App.jsx
+++ Client_Site/src/App.jsx
@@ -1,9 +1,21 @@
-function App() {
+import { useQuery } from "@apollo/client";
+import { GET_USERS } from "../graphqlQuery";
+
+export default function App() {
+  const { loading, error, data } = useQuery(GET_USERS);
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error: {error.message}</p>;
+
   return (
-    <div className="text-center text-xl my-10">
-      <h1>User Details</h1>
-    </div>
+    <ul>
+      {data.getUsers.map((item) => (
+        <>
+          <li>{item.id}</li>
+          <li>{item.name}</li>
+          <li>{item.age}</li>
+          <li>{item.isMarried}</li>
+        </>
+      ))}
+    </ul>
   );
 }
-
-export default App;
